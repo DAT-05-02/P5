4822c214c1b7e9527103a512090c0e8083e5f422
khm <kh.mikkelsen@hotmail.com>
2023-09-17 11:58:17 +0200

    Refractor to not conflict with pytest built-in 'setup'.

--- core/run.py
+++ core/run.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 from core import log
-from core.data.fetch import fetch_images, setup
+from core.data.fetch import fetch_images, setup_dataset
 
 
 if __name__ == "__main__":
-    df = setup("leopidotera-dk/multimedia.txt", "leopidotera-dk.csv")
+    df = setup_dataset("leopidotera-dk/multimedia.txt", "leopidotera-dk.csv")
     #
     # Uncomment below depending on how many images you want to download
     #
