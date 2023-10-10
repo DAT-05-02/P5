50f716d20c6cdac68f14493505d9c4ec8d8ea108
Lucas2k <94834530+lucas2000k@users.noreply.github.com>
2023-10-10 15:06:24 +0200

    changed call parameters in run.py back
    whoops

--- core/run.py
+++ core/run.py
@@ -5,5 +5,5 @@ LABEL_PATH="leopidotera-dk/occurrence.txt"
 if __name__ == "__main__":
     #put specific species in and uncomment to filter dataset
     #chosenButterflies = ["Aglais io", "Aglais urticae"]
-    df = setup_dataset("leopidotera-dk/multimedia.txt", LABEL_PATH, DATA_PATH, None, True)
+    df = setup_dataset("leopidotera-dk/multimedia.txt", LABEL_PATH, DATA_PATH, 50)
     #fetch_images(df, "identifier")
