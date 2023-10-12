0bcfd260b7d98eb65ee3489fadcb64452a43ad29
Lucas2k <94834530+lucas2000k@users.noreply.github.com>
2023-10-12 08:41:02 +0200

    fetches again
    whoops again

--- core/run.py
+++ core/run.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     #put specific species in and uncomment to filter dataset
     #chosenButterflies = ["Aglais io", "Aglais urticae"]
     df = setup_dataset("leopidotera-dk/multimedia.txt", LABEL_PATH, DATA_PATH, 50)
-    #fetch_images(df, "identifier")
+    fetch_images(df, "identifier")
