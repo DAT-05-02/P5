c0f0fa0dae62e1c1f61a9f45a87d092d113ca113
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-10-25 16:38:44 +0200

    update run

--- core/run.py
+++ core/run.py
@@ -14,11 +14,15 @@ if __name__ == "__main__":
                        raw_label_path=RAW_LABEL_PATH,
                        label_dataset_path=LABEL_DATASET_PATH,
                        dataset_csv_filename=DATASET_PATH,
-                       num_rows=200,
+                       num_rows=1000,
                        bfly=["all"])
     fetch_images(df, "identifier")
     #ft_extractor = FeatureExtractor()
     #df = ft_extractor.pre_process(df, "lbp", radius=7, should_bb=True, should_resize=True)
     model = Model(df)
-    model.model_compile_fit_evaluate(epochs=10)
+    model.print_dataset_info()
+    model.compile()
+    model.split_dataset()
+    model.fit(3)
+    model.evaluate()
 
