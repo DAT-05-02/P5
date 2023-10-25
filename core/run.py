92b32cdc27328d25032071f6e3f10f8763138eac
khmikkelsen <kh.mikkelsen@hotmail.com>
2023-10-25 11:15:17 +0200

    chdir fix for running in terminal

--- core/run.py
+++ core/run.py
@@ -9,16 +9,17 @@ from core.model.model import Model
 
 if __name__ == "__main__":
     sys.path.append(f"{os.getcwd()}{os.sep}core")
-    os.chdir("core")
+    if not os.getcwd().split(os.sep)[-1] == "core":
+        os.chdir("core")
     df = setup_dataset(raw_dataset_path=RAW_DATA_PATH,
                        raw_label_path=RAW_LABEL_PATH,
                        label_dataset_path=LABEL_DATASET_PATH,
                        dataset_csv_filename=DATASET_PATH,
                        num_rows=50,
-                       bfly=["all"])
+                       bfly="all")
     fetch_images(df, "identifier")
     ft_extractor = FeatureExtractor()
     df = ft_extractor.pre_process(df, "lbp", radius=7, should_bb=True, should_resize=True)
     model = Model(df)
-    model.model_compile_fit_evaluate(epochs=300)
+    model.model_compile_fit_evaluate(epochs=50)
 
