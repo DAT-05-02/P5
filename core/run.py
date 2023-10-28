57b5a39f2e5a17804941f39f8f99c869bcdb6217
khmikkelsen <kh.mikkelsen@hotmail.com>
2023-10-28 11:50:52 +0200

    Bump to python3.8 Helper class for to change work dir and check correct python is installed.

--- core/run.py
+++ core/run.py
@@ -1,16 +1,12 @@
-import os
-import sys
 
 from core.data.fetch import fetch_images, setup_dataset
 from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, DATASET_PATH, LABEL_DATASET_PATH, IMGDIR_PATH
 from core.data.feature import FeatureExtractor
 from core.model.model import Model
-
+from util.pysetup import PySetup
 
 if __name__ == "__main__":
-    sys.path.append(f"{os.getcwd()}{os.sep}core")
-    if not os.getcwd().split(os.sep)[-1] == "core":
-        os.chdir("core")
+    ops = PySetup()
     df = setup_dataset(raw_dataset_path=RAW_DATA_PATH,
                        raw_label_path=RAW_LABEL_PATH,
                        label_dataset_path=LABEL_DATASET_PATH,
