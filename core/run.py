49271cfb7b426361f71ad159ead7fcfd8a452471
thomaskettunen <49203257+thomaskettunen@users.noreply.github.com>
2023-10-31 10:45:41 +0100

    Created function for checking that all shapes in feature folders are the same

--- core/run.py
+++ core/run.py
@@ -4,25 +4,29 @@ import sys
 from core.data.fetch import fetch_images, setup_dataset
 from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, DATASET_PATH, LABEL_DATASET_PATH, IMGDIR_PATH
 from core.data.feature import FeatureExtractor
-# from core.model.model import Model
+from core.model.model import Model
 
 
 if __name__ == "__main__":
-    sys.path.append(f"{os.getcwd()}/core")
+    sys.path.append(f"{os.getcwd()}{os.sep}core")
     if not os.getcwd().split(os.sep)[-1] == "core":
         os.chdir("core")
-    print(os.getcwd())
     df = setup_dataset(raw_dataset_path=RAW_DATA_PATH,
                        raw_label_path=RAW_LABEL_PATH,
                        label_dataset_path=LABEL_DATASET_PATH,
                        dataset_csv_filename=DATASET_PATH,
-                       num_rows=10,
+                       num_rows=50,
                        bfly=["all"])
     fetch_images(df, "identifier")
-    #FeatureExtractor.create_augmented_images(IMGDIR_PATH)
+    FeatureExtractor.create_augmented_images(IMGDIR_PATH)
     ft_extractor = FeatureExtractor()
-    df = ft_extractor.pre_process(df, "sift", radius=7, should_bb=True, should_resize=True)
-    ft_extractor.feature_output_same_checker(feature="sift", df=df)
-    #model = Model(df)
-    #model.model_compile_fit_evaluate(epochs=50)
-
+    df = ft_extractor.pre_process(
+        df, "lbp", radius=7, should_bb=True, should_resize=True)
+    model = Model(df)
+    # model.load()
+    model.print_dataset_info()
+    model.compile()
+    model.split_dataset()
+    model.fit(5)  # Epochs
+    model.save()
+    model.evaluate_and_show_predictions()
