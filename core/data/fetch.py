64570e3f101af1a177b9707f2a4188f0d56f584a
thomaskettunen <49203257+thomaskettunen@users.noreply.github.com>
2023-11-02 14:44:00 +0100

    Added the global butterfly datasets

--- core/data/fetch.py
+++ core/data/fetch.py
@@ -97,9 +97,7 @@ def pad_dataset(df, raw_dataset_path: str, raw_label_path: str, csv_path: str, m
 def drop_cols(dfs, cols):
     print("where are we dropping boys")
     for df in list(dfs):
-        print("yo", df)
         df.drop(columns=[col for col in df if col not in MERGE_COLS], inplace=True)
-        print("yo2", df)
 
 
 def img_path_from_row(row: pd.Series, index: int, column="identifier", extra=None):
