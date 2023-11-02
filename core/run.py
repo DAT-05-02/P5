335a12a8983ed4a8b5e4f8bcd110d627d7c54b8e
dat <kh.mikkelsen@hotmail.com>
2023-11-02 13:19:42 +0100

    refactored fetch.py to be a Database class, which aggregates num_rows based on what it's settings are. This is because flipped images will be new rows. Could probably include additional checks, checking whether the naming of files correspond to database._num_rows.

--- core/run.py
+++ core/run.py
@@ -2,24 +2,26 @@ import logging
 import os
 import sys
 
-from core.data.fetch import fetch_images, setup_dataset
+from core.data.fetch import Database
 from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, DATASET_PATH, LABEL_DATASET_PATH
 from core.data.feature import FeatureExtractor
 from core.model.model import Model
-
 
 if __name__ == "__main__":
     sys.path.append(f"{os.getcwd()}{os.sep}core")
     if not os.getcwd().split(os.sep)[-1] == "core":
         os.chdir("core")
-    df = setup_dataset(raw_dataset_path=RAW_DATA_PATH,
-                       raw_label_path=RAW_LABEL_PATH,
-                       label_dataset_path=LABEL_DATASET_PATH,
-                       dataset_csv_filename=DATASET_PATH,
-                       num_rows=50,
-                       bfly=["all"])
-    fetch_images(df, "identifier")
     ft_extractor = FeatureExtractor(log_level=logging.DEBUG)
+    db = Database(raw_dataset_path=RAW_DATA_PATH,
+                  raw_label_path=RAW_LABEL_PATH,
+                  label_dataset_path=LABEL_DATASET_PATH,
+                  dataset_csv_filename=DATASET_PATH,
+                  ft_extractor=ft_extractor,
+                  num_rows=50,
+                  degrees="all",
+                  bfly=["all"])
+    df = db.setup_dataset()
+    df = db.fetch_images(df, "identifier")
     df = ft_extractor.pre_process(df, "lbp", radius=2)
     model = Model(df)
     # model.load()
