5f114fe3af46127a7e9919538efaac91c20bb809
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-11-07 13:24:13 +0100

    add: yolo_accepted column based on yolo detection

--- core/yolo/run_yolo.py
+++ core/yolo/run_yolo.py
@@ -1,33 +1,37 @@
 import os
 from PIL import Image
+import numpy as np
 
 from .yolo_func import obj_det, yolo_crop
 import time
 
 
-def run_yolo(model, root_path) -> None:
+def run_yolo(model, df, root_path) -> None:
+    print("======= RUN YOLO START ======")
     start_time = time.time()
     overwrite_count = 0
     delete_count = 0
 
-    for dirpath in os.scandir(root_path):
-        imgdir_name = os.path.splitext(f"{dirpath.name}")[0]
-        if len(os.listdir(f"{root_path}/{imgdir_name}")) > 0:
-            for imgpath in os.scandir(f"{root_path}/{imgdir_name}"):
-                img_name = os.path.splitext(f"{imgpath.name}")[0]
-                img_type = os.path.splitext(f"{imgpath.name}")[1]
-                img = Image.open(f"{root_path}/{imgdir_name}/{img_name}{img_type}")
-                res = obj_det(img, model)
-                xywhn = res[0].boxes.xywhn
-                if xywhn.numel() > 0:
-                    img1 = yolo_crop(img, xywhn)
-                    img1.save(res[0].path)
-                    overwrite_count += 1
-                else:
-                    print(f"deleting {res[0].path}")
-                    os.remove(res[0].path)
-                    delete_count += 1
+    yolo_accepted = [True for x in range(len(df.index))]
+
+    for idx, row in df.iterrows():
+        print(row["path"])
+        img = Image.fromarray(np.load(row["path"]))
+        res = obj_det(img, model)
+        xywhn = res[0].boxes.xywhn
+        if xywhn.numel() > 0:
+            img1 = yolo_crop(img, xywhn)
+            img1.save(res[0].path)
+            overwrite_count += 1
+        else:
+            print(f"deleting {res[0].path}")
+            #os.remove(res[0].path)
+            yolo_accepted[idx] = False
+            delete_count += 1
+
+    df["yolo_accepted"] = yolo_accepted
 
     total_img = overwrite_count + delete_count
     print(f"Deleted {delete_count} out of {total_img} total images.")
     print("--- Took %s seconds ---" % (time.time() - start_time))
+    print("======= RUN YOLO DONE ======")
