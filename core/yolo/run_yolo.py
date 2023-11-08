a82bf5ecd3bf62526a424be6448e49de2591e414
khmikkelsen <kh.mikkelsen@hotmail.com>
2023-11-08 21:43:30 +0100

    leaving run_yolo for maybe later use, but for now obj_det is called in fetch.

--- core/yolo/run_yolo.py
+++ core/yolo/run_yolo.py
@@ -1,18 +1,23 @@
-import os
+import pandas as pd
 from PIL import Image
 import numpy as np
+from ultralytics import YOLO
 
 from .yolo_func import obj_det, yolo_crop
 import time
 
 
-def run_yolo(model, df) -> None:
+def run_yolo(df) -> pd.DataFrame:
+    if 'yolo_accepted' in df:
+        return df
+
+    model = YOLO('yolo/medium250e.pt')
     print("======= RUN YOLO START ======")
     start_time = time.time()
     overwrite_count = 0
     delete_count = 0
 
-    yolo_accepted = [True for x in range(len(df.index))]
+    yolo_accepted = np.full(len(df.index), fill_value=np.nan).tolist()
 
     for idx, row in df.iterrows():
         print(row["path"])
@@ -23,6 +28,7 @@ def run_yolo(model, df) -> None:
             img1 = yolo_crop(img, xywhn)
             img1.save(res[0].path)
             overwrite_count += 1
+            yolo_accepted[idx] = True
         else:
             print(f"deleting {res[0].path}")
             #os.remove(res[0].path)
@@ -30,11 +36,12 @@ def run_yolo(model, df) -> None:
             delete_count += 1
 
     df["yolo_accepted"] = yolo_accepted
+    df.dropna(subset=['yolo_accepted'], inplace=True)
+    df.reset_index(inplace=True, drop=True)
 
     total_img = overwrite_count + delete_count
     print(f"Deleted {delete_count} out of {total_img} total images.")
     print("--- Took %s seconds ---" % (time.time() - start_time))
     print("======= RUN YOLO DONE ======")
 
-    new_df = df[df["yolo_accepted"] is True] # Drops all rows that doesn't satisfy yolo accepted
-    return new_df
+    return df
