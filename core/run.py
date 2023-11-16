bc3c9975f597f5d926fb4fcc1eddf36d5ed90146
Lucas2k <lucasmartin2k@gmail.com>
2023-11-16 12:35:54 +0100

    Added argparse, use 'python -m core.run --help' for description of args. args get saved in core.util.constants

--- core/run.py
+++ core/run.py
@@ -1,14 +1,26 @@
 import logging
+import core
 
 from core.data.fetch import Database
-from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, DATASET_PATH, LABEL_DATASET_PATH, IMGDIR_PATH
+from core.util.constants import (RAW_DATA_PATH, RAW_LABEL_PATH, DATASET_PATH, LABEL_DATASET_PATH, IMGDIR_PATH,
+                                 NUM_IMAGES, CROPPED, KERNEL_SIZE, NUM_EPOCHS)
 from core.data.feature import FeatureExtractor
 from core.model.model import Model
 from core.util.pysetup import PySetup
+from core.util.util import setup_argparse
 
 if __name__ == "__main__":
+    args = setup_argparse()
+    core.util.constants.MODEL_ID = args.MODEL_ID
+    core.util.constants.KERNEL_SIZE = args.KERNEL_SIZE
+    core.util.constants.LEARNING_RATE = args.LEARNING_RATE
+    core.util.constants.NUM_EPOCHS = args.NUM_EPOCHS
+    core.util.constants.NUM_IMAGES = args.NUM_IMAGES
+    core.util.constants.IMG_SIZE = args.IMG_SIZE
+    core.util.constants.CROPPED = args.CROPPED
+
     ops = PySetup()
-    num_rows = 6000
+    num_rows = NUM_IMAGES
     feature = ""
     ft_extractor = FeatureExtractor(log_level=logging.INFO)
     db = Database(raw_dataset_path=RAW_DATA_PATH,
@@ -18,7 +30,7 @@ if __name__ == "__main__":
                   log_level=logging.DEBUG,
                   ft_extractor=ft_extractor,
                   num_rows=num_rows,
-                  crop=False,
+                  crop=CROPPED,
                   minimum_images=None,
                   degrees="none",
                   bfly=["all"])
@@ -26,11 +38,11 @@ if __name__ == "__main__":
     df = ft_extractor.pre_process(df, feature, radius=2)
 
     df = db.only_accepted(df)
-    model = Model(df, IMGDIR_PATH, feature=feature, kernel_size=(3, 3))
+    model = Model(df, IMGDIR_PATH, feature=feature, kernel_size=(KERNEL_SIZE, KERNEL_SIZE))
     # model.load()
     # model.print_dataset_info()
     model.compile()
     model.split_dataset()
-    model.fit(50)  # Epochs
+    model.fit(NUM_EPOCHS)
     model.save()
     # model.evaluate_and_show_predictions(num_samples=3)
