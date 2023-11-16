5c60b8f94140e76049aa41579ce01d13c75b4dc0
Lucas2k <lucasmartin2k@gmail.com>
2023-11-16 13:59:48 +0100

    Saves argsparse in json instead of normal util constants

--- test/test_data/test_feature.py
+++ test/test_data/test_feature.py
@@ -1,20 +1,23 @@
 import logging
 import os
+import json
 
 import numpy as np
 import pytest
 import pandas as pd
 from core.data.feature import FeatureExtractor
 from core.data.fetch import Database
-from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, LABEL_DATASET_PATH, DATASET_PATH, IMG_SIZE
+from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, LABEL_DATASET_PATH, DATASET_PATH
 import shutil
 
+with open('core/util/constants.txt', 'r') as f:
+    constants = json.load(f)
 
 @pytest.fixture
 def temp_dir(request):
     temp_dir = "temp-test-dir/temp-species-dir"
     os.makedirs(temp_dir, exist_ok=True)
-    image = np.full((IMG_SIZE, IMG_SIZE, 3), fill_value=1, dtype=np.uint8)
+    image = np.full((constants["IMG_SIZE"], constants["IMG_SIZE"], 3), fill_value=1, dtype=np.uint8)
     logging.info(image.shape)
     np.save(f"{temp_dir}/test_0.npy", image)
 
