ece977215a9adc5205f12de57c04e43aa7627b9a
khmikkelsen <kh.mikkelsen@hotmail.com>
2023-11-21 16:59:31 +0100

    Convert constants to Singleton class

--- core/run.py
+++ core/run.py
@@ -1,32 +1,14 @@
 import logging
-import json
 
 from core.data.fetch import Database
 from core.util.constants import RAW_DATA_PATH, RAW_LABEL_PATH, DATASET_PATH, LABEL_DATASET_PATH, IMGDIR_PATH
 from core.data.feature import FeatureExtractor
 from core.model.model import Model
 from core.util.pysetup import PySetup
-from core.util.util import setup_argparse
+from core.util.util import ConstantSingleton
 
 if __name__ == "__main__":
-    args = setup_argparse()
-    if args is not None:
-        json_constants = {
-            "MODEL_ID": args.MODEL_ID,
-            "KERNEL_SIZE": args.KERNEL_SIZE,
-            "LEARNING_RATE": args.LEARNING_RATE,
-            "NUM_EPOCHS": args.NUM_EPOCHS,
-            "NUM_IMAGES": args.NUM_IMAGES,
-            "IMG_SIZE": args.IMG_SIZE,
-            "CROPPED": args.CROPPED
-        }
-        with open('core/util/constants.txt', 'w') as f:
-            json.dump(json_constants, f)
-
-    with open('core/util/constants.txt', 'r') as f:
-        constants = json.load(f)
-
-
+    constants = ConstantSingleton()
     ops = PySetup()
     num_rows = constants["NUM_IMAGES"]
     feature = ""
@@ -44,7 +26,6 @@ if __name__ == "__main__":
                   bfly=["all"])
     df = db.setup_dataset()
     df = ft_extractor.pre_process(df, feature, radius=2)
-
     df = db.only_accepted(df)
     model = Model(df, IMGDIR_PATH, feature=feature, kernel_size=(constants["KERNEL_SIZE"], constants["KERNEL_SIZE"]))
     # model.load()
